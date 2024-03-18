0082ffafaeff9d6463fdf6d4de58a93ba0b50da0
Anosha Sohail <anoshasohail676@gmail.com>
2024-03-18 14:44:50 +0500

    task manager app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="all-tasks">
+    <AllTasks></AllTasks>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import AllTasks from "./components/AllTasks.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AllTasks,
+  },
+};
 </script>
 
 <style>
+@import './assets/style.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /* -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form action="">
+      <input type="text" v-model="newTask" />
+      <button @click="addTask">Add Task</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+   props: ['tasks'],
+  data(){
+    return{
+        newTask:''
+    }
+  },
+  methods:{
+    addTask(){
+        if(this.newTask.trim()!==''){
+            this.$emit('add-task', this.newTask);
+            this.newTask='';
+        }
+    }
+  }
+};
+</script>
